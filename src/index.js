0c08a6f8ec4dd38d882193a6d7ccafb48ff11b2c
Ryan <coap.ryanmontgomery@gmail.com>
2024-06-19 14:54:55 -0600

    routes! enfin!

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+// Step 1. Import react-router functions
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
+import About from "./pages/About";
+import Login from "./pages/Login";
+import UserProfile from "./pages/UserProfile";
+import ErrorPage from "./pages/ErrorPage";
+import routes from "./routes";
+
+const router = createBrowserRouter(routes);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Home />);
+root.render(<RouterProvider router={router} />);
